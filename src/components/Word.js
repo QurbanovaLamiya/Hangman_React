791de566710e5382b414e231f19ecba0db910ac1
QurbanovaLamiya <lamiyaqurbanova121@gmail.com>
2023-03-01 00:17:09 +0400

    Update

--- src/components/Word.js
+++ src/components/Word.js
@@ -1,5 +1,13 @@
 const Word = ({ selectedWord, correctLetters }) => {
-  return <div className="word"></div>;
+  return (
+    <div className="word">
+      {selectedWord.split("").map((letter, index) => (
+        <span className="letter" key={index}>
+          {correctLetters.includes(letter) ? letter : ""}
+        </span>
+      ))}
+    </div>
+  );
 };
 
 export default Word;
